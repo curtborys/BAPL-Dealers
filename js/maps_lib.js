1dda90d4c32b234d5313fa07baaad5677a717390
curtborys <curt.borys@gmail.com>
2016-11-09 16:12:00 -0600

    Update maps_lib.js

--- js/maps_lib.js
+++ js/maps_lib.js
@@ -242,7 +242,7 @@
     var searchType = type_column + " IN (-1,";
     if ( $("#cbType100").is(':checked')) searchType += "1,"; 
 	else if (!$("#cbType100").is(':checked'))
-		$(":checkbox[name='Canada[]']").attr("checked", false);
+		$(":checkbox[name='canada[]']").attr("checked", false);
     if ( $("#cbType101").is(':checked')) searchType += "2,";
     self.whereClause += " AND " + searchType.slice(0, searchType.length - 1) + ")";        
         //-----end of custom filters-----
