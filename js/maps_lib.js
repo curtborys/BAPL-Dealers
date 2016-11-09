06f4ecffbbafa8763a02937a6b0cb59616b4c109
curtborys <curt.borys@gmail.com>
2016-11-09 16:48:18 -0600

    Update maps_lib.js

--- js/maps_lib.js
+++ js/maps_lib.js
@@ -241,6 +241,8 @@
     var type_column = "'SearchType3'";
     var searchType = type_column + " IN (-1,";
     if ( $("#cbType100").is(':checked')) searchType += "1,"; 
+    else if ($("#cbType100").is(':checked'))
+	$(":checkbox[name='canada[]']").attr("checked", true);
     else if (!$("#cbType100").is(':checked'))
 	$(":checkbox[name='canada[]']").attr("checked", false);
     if ( $("#cbType101").is(':checked')) searchType += "2,";
